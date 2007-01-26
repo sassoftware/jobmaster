e838e39708bffaeea58b26d568a2d042dd69d008
Stu Gott <http://issues.rpath.com/>
2007-01-26 13:43:41 -0500

    flesh out __init__ contents

---
+++ jobmaster/__init__.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2006-2007 rPath, Inc.
+#
+# All rights reserved
+#
+"""
+Job Master Daemon
+
+@group Daemon: imagecache.py master_error.py master.py slavehandler.py xencfg.py xenmac.py
+"""
