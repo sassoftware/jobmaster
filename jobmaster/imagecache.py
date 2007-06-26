c60ff49ed737c7135a0f7f9e5c0a75c5df6ab71c
Tim Gerla <http://issues.rpath.com/>
2007-06-26 15:57:14 -0400

    correct preload wrapper path

--- jobmaster/imagecache.py
+++ jobmaster/imagecache.py
@@ -245,7 +245,7 @@ class ImageCache(object):
             #os.system("conary update --sync-to-parents kernel:runtime "
             #          "--root %s" % mntDir)
 
-            safeEnv = {"LD_PRELOAD": "/usr/lib/rbuilder/chrootsafe_wrapper.so"}
+            safeEnv = {"LD_PRELOAD": "/usr/lib/jobmaster/chrootsafe_wrapper.so"}
             logCall("chroot %s /usr/bin/authconfig --kickstart --enablemd5 --enableshadow --disablecache" % mntDir, env = safeEnv)
             logCall("chroot %s /usr/sbin/usermod -p '' root" % mntDir)
             logCall('grubby --remove-kernel=/boot/vmlinuz-template --config-file=%s' % os.path.join(mntDir, 'boot', 'grub', 'grub.conf'))
