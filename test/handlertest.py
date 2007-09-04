13d56695e42ea7a4b22e0c284ab90aa767914d20
Tim Gerla <http://issues.rpath.com/>
2007-09-04 16:06:10 -0400

    use external project credentials when estimating scratch space (RBL-2075)

--- test/handlertest.py
+++ test/handlertest.py
@@ -184,7 +184,7 @@ class HandlerTest(jobmaster_helper.JobMasterHelper):
             util.rmtree(cfgPath, ignore_errors = True)
             master.getIP = getIP
 
-    def testEsitmateTroveSize(self):
+    def testEstimateTroveSize(self):
         troveName = 'group-test'
         troveVersion = '/test.rpath.local@rpl:1/0.0:1-1-1'
         troveFlavor = '1#x86'
@@ -192,20 +192,22 @@ class HandlerTest(jobmaster_helper.JobMasterHelper):
         handler = master.SlaveHandler(self.jobMaster, troveSpec,
                 {'UUID' : 'test.rpath.local-build-55', 'troveName': troveName,
                     'troveVersion': troveVersion, 'troveFlavor': troveFlavor,
-                    'protocolVersion': 1})
+                    'protocolVersion': 1, 'project': {'conaryCfg': 'name RonaldFrobnitz'}})
 
         handler.slaveName = 'xen44'
 
         ConaryClient = conaryclient.ConaryClient
         class MockClient(object):
-            def __init__(x):
+            def __init__(x, y = None):
                 x.getRepos = lambda *args, **kwargs: x
                 x.findTrove = lambda *args, **kwargs: [[x]]
                 x.getTrove = lambda *args, **kwargs: x
                 x.troveInfo = x
-                x.cfg = x
+                x.cfg = y
                 x.flavor = None
                 x.size = lambda *args, **kwargs: 55 * 1024 * 1024 # 55M trove
+
+                self.failUnlessEqual(y.name, "RonaldFrobnitz")
         try:
             conaryclient.ConaryClient = MockClient
             res = handler.estimateScratchSize()
