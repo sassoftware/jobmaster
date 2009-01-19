e469ce80cb45505f15b998b87273d190070d56f2
Michael Tharp <mtharp@rpath.com>
2009-01-19 16:43:16 +0000

    Update to use new chain for proxy rules. (RBL-4116)

--- proxy-firewall.py
+++ proxy-firewall.py
@@ -12,6 +12,8 @@ from conary.conarycfg import ConaryConfiguration
 def main(args):
     cfg = ConaryConfiguration(False)
     cfg.read('/etc/conaryrc', exception=False)
+
+    holes = set()
     for schema, uri in cfg.proxy.items():
         userhostport = urlparse.urlsplit(uri)[1]
         hostport = urllib.splituser(userhostport)[1]
@@ -21,9 +23,15 @@ def main(args):
                 port = '443'
             else:
                 port = '80'
-        os.system('/sbin/iptables -A FORWARD-SLAVE -m state --state NEW '
-                '-m tcp -p tcp --dport %s -d %s -j ACCEPT 2>/dev/null'
-                % (port, host))
+        holes.add((host, port))
+
+    if os.system('/sbin/iptables -F FORWARD-PROXY'):
+        print >> sys.stderr, "Failed to flush existing proxy chain"
+        return 1
+
+    for host, port in holes:
+        os.system('/sbin/iptables -A FORWARD-PROXY -m state --state NEW '
+                '-m tcp -p tcp --dport %s -d %s -j ACCEPT' % (port, host))
 
 
 if __name__ == '__main__':
